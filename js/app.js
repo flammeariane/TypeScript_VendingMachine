1159cf7474c318b84b12cc56754f48be1011833a
flamme <aflamme@actiris.be>
2020-01-31 16:50:59 +0100

    Understanding Basics => Array

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var Quarter = /** @class */ (function () {
+    function Quarter() {
+        this.value = 0.25;
+    }
+    Object.defineProperty(Quarter.prototype, "Value", {
+        get: function () {
+            return this.value;
+        },
+        set: function (newValue) {
+            this.value = newValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Quarter.prototype.getImageUrl = function () {
+        return "img/bitcoin.png";
+    };
+    return Quarter;
+}());
+/// <reference path="./coin.ts" />
+var VendingMachine = /** @class */ (function () {
+    function VendingMachine() {
+        var _this = this;
+        this.paid = ko.observable(0);
+        this.acceptedCoins = [new Quarter()];
+        this.acceptCoin = function (coin) {
+            var oldTotal = _this.paid();
+            _this.paid(oldTotal + coin.Value);
+        };
+    }
+    return VendingMachine;
+}());
+/// <reference path="vendingMaching.ts"/>
+/// <reference path="typings/knockout.d.ts" />
+var machine = new VendingMachine();
+ko.applyBindings(machine);
+//# sourceMappingURL=app.js.map
---
+++ ts/bootstrapper.ts
@@ -0,0 +1,5 @@
+/// <reference path="vendingMaching.ts"/>
+/// <reference path="typings/knockout.d.ts" />
+
+var machine = new VendingMachine();
+ko.applyBindings(machine);
